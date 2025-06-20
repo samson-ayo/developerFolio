a3992f6b4a7211e502757759ad12476fcc2475f2
Samson Ayodele <olotusamson772@gmail.com>
2025-06-20 18:53:10 +0100

    Enable reCAPTCHA on contact form

--- src/containers/contact/Contact.js
+++ src/containers/contact/Contact.js
@@ -77,6 +77,7 @@ export default function Contact() {
                 name="contact"
                 method="POST"
                 data-netlify="true"
+                data-netlify-recaptcha="true"
                 data-netlify-honeypot="bot-field"
                 onSubmit={handleSubmit}
                 className="contact-form"
